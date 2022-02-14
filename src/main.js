6c5665355a4c6e015a6cbc08882077fd88927d10
svaticalm <svaticalm@gmail.com>
2022-02-14 23:23:35 +0300

    Шрифты

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
-import './assets/sass/style.sass';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+// STYLES
+import './assets/sass/style.sass';
 
 createApp(App).use(store).use(router).mount('#app');
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="content">
-        das
-        <div class="">
-            samp
+        <div class="circle"></div>
+        <div class="df df--space-between">
+            <div class="logo">
+                Spotiswipe
+            </div>
+            <div class="setting-btn"></div>
+        </div>
+        <div class="cards">
+            <div class="cards__item active"></div>
+            <div class="cards__item"></div>
         </div>
     </div>
 </template>
